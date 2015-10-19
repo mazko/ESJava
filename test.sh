df10d622ef94f22b1037d086df7ed75a74f2d0b5
Oleg Mazko <o.mazko@mail.ru>
2015-10-19 13:45:29 +0300

    test dir

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-coffee --nodejs '--stack-size=10000' src/cmd.coffee Test.java \
-> Test.es6.js && \
+coffee --nodejs '--stack-size=10000' src/cmd.coffee test/Test.java \
+> test/Test.es6.js && \
 # babel --blacklist strict Test.es6.js > Test.js
-babel Test.es6.js > Test.js #&& \
+babel test/Test.es6.js > test/Test.js #&& \
 # jslint --edition=es6 Test.es6.js
