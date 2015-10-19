41331af0b7632b2112ca00c721c9002ca5b393cf
Oleg Mazko <o.mazko@mail.ru>
2015-10-19 13:02:58 +0300

    parser update

--- parse.js
+++ parse.js
@@ -48,7 +48,7 @@ function parse(delay) {
         } catch (err) {
             str = !err.name 
                 ? err : err.name === 'SyntaxError' 
-                ? "Line " + err.line + ", column " + err.column + "\n" + err
+                ? "Location: " + JSON.stringify(err.location, null, 4) + "\n" + err
                 : err.name + ': ' + err.message;
             id('info').innerHTML = str;
             id('info').className = 'alert-box alert';
