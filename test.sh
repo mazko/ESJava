f314828bf74e85adf1c7d6e0d66840e37689c450
Oleg Mazko <o.mazko@mail.ru>
2015-10-19 13:41:32 +0300

    node stack size

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
-coffee src/cmd.coffee Test.java > Test.es6.js && \
+coffee --nodejs '--stack-size=10000' src/cmd.coffee Test.java \
+> Test.es6.js && \
 # babel --blacklist strict Test.es6.js > Test.js
 babel Test.es6.js > Test.js #&& \
 # jslint --edition=es6 Test.es6.js
