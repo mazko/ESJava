1441bb446f8baf2c48a2c103ceabe4a25d748a5d
Oleg Mazko <o.mazko@mail.ru>
2015-12-11 13:14:55 +0200

    test before publ

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 test/test.sh && \
-git status --porcelain | grep -q . && { echo 'git diff test FAILED !!!' ; exit 1; } || \
 browserify -t coffeeify --extension=".coffee" --standalone javaconves6func src/ESJava.coffee > bundle.js
--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,6 @@
 coffee --nodejs '--stack-size=10000' src/cmd.coffee test/Test.java \
 > test/Test.es6.js && \
 # babel --blacklist strict Test.es6.js > Test.js
-babel --presets es2015 test/Test.es6.js > test/Test.js #&& \
+babel --presets es2015 test/Test.es6.js > test/Test.js && \
 # jslint --edition=es6 Test.es6.js
+git status --porcelain | grep -q . && { echo 'git diff test FAILED !!!' ; exit 1; }
