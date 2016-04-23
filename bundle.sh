1f9b2d0bb9b287b682548db7e0df8670761fdcf7
Oleg Mazko <o.mazko@mail.ru>
2016-04-23 13:50:25 +0300

    demo uglifyjs

--- bundle.sh
+++ bundle.sh
@@ -1,2 +1,5 @@
 test/test.sh && \
-./node_modules/browserify/bin/cmd.js -t coffeeify --extension=".coffee" --standalone javaconves6func src/ESJava.coffee > bundle.js
+./node_modules/browserify/bin/cmd.js \
+-t coffeeify --extension=".coffee" \
+ --standalone javaconves6func src/ESJava.coffee | \
+ ./node_modules/uglify-js/bin/uglifyjs -c > bundle.js
